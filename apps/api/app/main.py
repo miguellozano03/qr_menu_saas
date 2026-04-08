c40099ee2703cf8200e07a5a8f409b362cf79b26
miguellozano03 <miguel.lozano.vergara@gmail.com>
2026-04-08 14:51:52 -0500

    feat: connect routes and exception handler to the api

--- apps/api/app/main.py
+++ apps/api/app/main.py
@@ -1,11 +1,9 @@
 from fastapi import FastAPI
 from app.core.config import settings
+from app.api.v1.router import api_router
+from app.core.db import models
+from app.core.exceptions_handlers import register_exceptions_handlers
 
 app = FastAPI(title=settings.app_name)
-
-@app.get("/info")
-async def info():
-    return {
-        "app_name": settings.app_name,
-        "version": 1.00,
-    }
+register_exceptions_handlers(app)
+app.include_router(api_router)
