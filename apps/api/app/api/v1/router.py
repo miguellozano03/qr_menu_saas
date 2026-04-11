eafbb1ea0fabf32f48d2bf1a9bdbf01c0bca7203
miguellozano03 <miguel.lozano.vergara@gmail.com>
2026-04-11 15:06:01 -0500

    feat: add routes

--- apps/api/app/api/v1/router.py
+++ apps/api/app/api/v1/router.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 from app.modules.users.router import auth_router, user_router
+from app.modules.restaurants.router import restaurant_router
 
 api_router = APIRouter(prefix="/v1")
 
 api_router.include_router(auth_router)
 api_router.include_router(user_router)
+api_router.include_router(restaurant_router)
