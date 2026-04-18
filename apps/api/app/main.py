72075b6c0e373822a4b7ba8b5eb8a85e0a4f088b
miguellozano03 <miguel.lozano.vergara@gmail.com>
2026-04-18 12:09:41 -0500

    feat: add contexto manager and mont the upload files for local development

--- apps/api/app/main.py
+++ apps/api/app/main.py
@@ -1,8 +1,21 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from app.core.config import settings
 from app.api.v1.router import api_router
 from app.core.exceptions_handlers import register_exceptions_handlers
+from app.shared.storage.local_setup import init_storage, UPLOAD_DIR
 
-app = FastAPI(title=settings.app_name)
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    if settings.environment == "development":
+        init_storage()
+    yield
+
+app = FastAPI(title=settings.app_name, lifespan=lifespan)
+
+if settings.environment == "development":
+    app.mount("/uploads", StaticFiles(directory=UPLOAD_DIR), name="uploads")
+
 register_exceptions_handlers(app)
 app.include_router(api_router, prefix='/api')
